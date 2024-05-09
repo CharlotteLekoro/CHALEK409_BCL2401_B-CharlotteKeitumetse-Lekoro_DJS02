41448d61881b5ff73ffcd02ef6919e9b08a2eca5
Charlotte Keitumetse Lekoro <156782118+CharlotteLekoro@users.noreply.github.com>
2024-05-09 21:12:05 +0200

    input check added
    Added an if else statement that checks it the either or both inputs are empty and returns a try again statement

--- scripts.js
+++ scripts.js
@@ -1,5 +1,4 @@
 
-
 const form = document.querySelector("[data-form]");
 const result = document.querySelector("[data-result]");
 
@@ -8,11 +7,16 @@ form.addEventListener("submit", (event) => {
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
 
-  // Perform the division
-  const quotient = dividend / divider;
+  // Check if either or both inputs are empty
+  if (dividend === "" || divider === "") {
+    result.innerText = "Division not performed. Both values are required in inputs. Try again.";
+  } else {
+    // Perform the division
+    const quotient = dividend / divider;
 
-  // Display the result without any decimal
-  result.innerText = quotient.toFixed(0);
+    // Display the result without any decimal
+    result.innerText = quotient.toFixed(0);
+  }
 });
 
 
